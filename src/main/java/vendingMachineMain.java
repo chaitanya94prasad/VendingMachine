4a483b8cf27f7d8e4ef7d57f1b655528eb3a1e82
Chaitanya Prasad <chprasad@SV-MAC-0187.local>
2022-11-08 22:13:23 +0530

    code

---
+++ src/main/java/vendingMachineMain.java
@@ -0,0 +1,8 @@
+import vending.VendingMachineImpl;
+
+public class vendingMachineMain {
+    public static void main(String[] args) {
+        VendingMachineImpl vendingMachineObj = new VendingMachineImpl();
+        vendingMachineObj.printStats();
+    }
+}
